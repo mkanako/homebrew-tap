262125055959ee1a22648c709791c1346970151d
kanako <4190346+mkanako@users.noreply.github.com>
2020-12-17 23:21:24 +0800

    new formula: mosdns

---
+++ mosdns.rb
@@ -0,0 +1,51 @@
+class Mosdns < Formula
+  desc "一个灵活的 DNS 转发器"
+  homepage "https://github.com/IrineSistiana/mosdns"
+  url "https://github.com/IrineSistiana/mosdns/releases/download/v0.16.0/mosdns-darwin-amd64.zip"
+  sha256 "73e36ac16f44a74c5bbb10a226c6caed24ad072413fcf7bb2d5e41289ca289d9"
+  license "GPLv3"
+
+  bottle :unneeded
+
+  def install
+    bin.install "mosdns"
+  end
+
+  def post_install
+    conf = etc/"mosdns/mosdns.yaml"
+    return unless !conf.exist?
+    system "mkdir", etc/"mosdns"
+    system bin/"mosdns", "-gen", conf
+  end
+
+  plist_options startup: true
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/mosdns</string>
+            <string>-dir</string>
+            <string>#{etc}/mosdns</string>
+            <string>-c</string>
+            <string>#{etc}/mosdns/mosdns.yaml</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/mosdns.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/mosdns.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
+end
