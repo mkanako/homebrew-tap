961646919183b5277dcf69c35e6e43d339647527
kanako <4190346+mkanako@users.noreply.github.com>
2020-12-19 14:52:34 +0800

    update 'update.sh'

--- update.sh
+++ update.sh
@@ -18,20 +18,20 @@ for formula_name in ${formula_names[@]}; do
     tag=$(curl -s -f https://api.github.com/repos/"$formula_name"/releases/latest | grep "tag_name" | cut -d '"' -f 4)
     if [ -z "$tag" ]; then
         echo "'tag_name' not found"
-        exit
+        exit 1
     fi
     echo "latest verison: ${tag}"
     verison=$(grep -wo $formula_file -e "${tag//[0-9]/\\d\?}" | head -1)
     if [ -z "$verison" ]; then
         echo "current verison not found"
-        exit
+        exit 1
     fi
     echo "current verison: ${verison}"
     if [ $verison != $tag ]; then
         url=$(cat $formula_file | ack 'url\s+"(http.*)"' --output "\$1" -)
         if [ -z "$url" ]; then
             echo "url not found"
-            exit
+            exit 1
         fi
         new_url=${url//${verison}/${tag}}
         hash=$(curl -L -f --progress-bar "$new_url" | shasum -a 256)
@@ -39,11 +39,15 @@ for formula_name in ${formula_names[@]}; do
             hash=${hash%% *}
             sed -i "" "s#${verison}#${tag}#g" $formula_file
             sed -i "" -e "s#\(sha256[[:blank:]]*\"\)[[:alnum:]]\{64\}#\1$hash#g" $formula_file
+            if [[ ! -z $RUN_ENV && $RUN_ENV == 'github' ]]; then
+                git commit $formula_file -m "${formula_name##*/}: update ${verison}"
+                git push origin master
+            fi
             echo "updated success"
         else
             echo $new_url
             echo 'get hash fail'
-            exit
+            exit 1
         fi
     fi
 done
