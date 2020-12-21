347713123e52f1aca4411d63013c8094c124b174
kanako <4190346+mkanako@users.noreply.github.com>
2020-12-21 13:58:53 +0800

    update 'update.sh'

--- update.sh
+++ update.sh
@@ -15,37 +15,37 @@ done
 for formula_name in ${formula_names[@]}; do
     echo $formula_name
     formula_file="${formula_name##*/}.rb"
-    tag=$(curl -s -f https://api.github.com/repos/"$formula_name"/releases/latest | grep "tag_name" | cut -d '"' -f 4)
-    if [ -z "$tag" ]; then
-        echo "'tag_name' not found"
+    latest_ver=$(curl -s -f https://api.github.com/repos/"$formula_name"/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+    if [ -z "$latest_ver" ]; then
+        echo "latest verison not found"
         exit 1
     fi
-    echo "latest verison: ${tag}"
-    verison=$(grep -wo $formula_file -e "${tag//[0-9]/\\d\?}" | head -1)
-    if [ -z "$verison" ]; then
+    echo "latest verison: ${latest_ver}"
+    current_ver=$(grep -wo $formula_file -e "${latest_ver//[0-9]/\\d\?}" | head -1)
+    if [ -z "$current_ver" ]; then
         echo "current verison not found"
         exit 1
     fi
-    echo "current verison: ${verison}"
-    if [ $verison != $tag ]; then
+    echo "current verison: ${current_ver}"
+    if [ $current_ver != $latest_ver ]; then
         url=$(cat $formula_file | ack 'url\s+"(http.*)"' --output "\$1" -)
         if [ -z "$url" ]; then
             echo "url not found"
             exit 1
         fi
-        new_url=${url//${verison}/${tag}}
-        hash=$(curl -L -f --progress-bar "$new_url" | shasum -a 256)
+        latest_url=${url//${current_ver}/${latest_ver}}
+        hash=$(curl -L -f --progress-bar "$latest_url" | shasum -a 256)
         if [[ $? -eq 0 && ! -z "$hash" && ${#hash} -gt 64 ]]; then
             hash=${hash%% *}
-            sed -i "" "s#${verison}#${tag}#g" $formula_file
+            sed -i "" "s#${current_ver}#${latest_ver}#g" $formula_file
             sed -i "" -e "s#\(sha256[[:blank:]]*\"\)[[:alnum:]]\{64\}#\1$hash#g" $formula_file
             if [[ ! -z $RUN_ENV && $RUN_ENV == 'github' ]]; then
-                git commit $formula_file -m "${formula_name##*/}: update ${tag}"
+                git commit $formula_file -m "${formula_name##*/}: update ${latest_ver}"
                 git push origin master
             fi
             echo "updated success"
         else
-            echo $new_url
+            echo $latest_url
             echo 'get hash fail'
             exit 1
         fi
